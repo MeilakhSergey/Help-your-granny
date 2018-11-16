4e8998618e122bb35d0a8d119f707b50076d09c4
MeilakhSergey <meylakhsergey@gmail.com>
2018-11-16 14:54:02 +0300

    It works!

--- src/Tour.java
+++ src/Tour.java
@@ -11,7 +11,26 @@ public class Tour {
         System.out.println(Tour.tour(friends1, fTowns1, distTable1)); //889
     }
     public static int tour(String[] arrFriends, String[][] ftwns, Map<String, Double> h) {
-        // your code
-        return 1;
+        Double[] distances = new Double[arrFriends.length];
+        for (int i = 0; i < arrFriends.length; i++) {
+            for (int j = 0; j < ftwns.length; j++) {
+                if (ftwns[j][0] == arrFriends[i]) {
+                    distances[i] = h.get(ftwns[j][1]);
+                    break;
+                }
+            }
+        }
+        double sum = distances[0];
+        for (int i = 1; i < distances.length; i++) {
+            if (distances[i] != null)
+                sum += Math.sqrt(distances[i]*distances[i] - distances[i-1]*distances[i-1]);
+        }
+        for (int i = distances.length - 1; i >=0; i--) {
+            if (distances[i] != null) {
+                sum += distances[i];
+                break;
+            }
+        }
+        return (int)Math.floor(sum);
     }
 }
